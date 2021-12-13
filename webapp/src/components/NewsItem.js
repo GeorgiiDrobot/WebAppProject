bed539c724125f167b96e3773c6398cea56fcd53
GeorgiiDrobot <57353814+GeorgiiDrobot@users.noreply.github.com>
2021-12-13 20:26:39 +0200

    api styled and date added

--- webapp/src/components/NewsItem.js
+++ webapp/src/components/NewsItem.js
@@ -2,12 +2,15 @@ import React from "react";
 import '../App.css'
 
 
-const NewsItem = ({title, description, url, urlToImg}) => {
+const NewsItem = ({title, description, url, urlToImage, publishedAt}) => {
+    
+    
     return (
         <div className='news-item'>
-            <img className='news-img' src={urlToImg} alt="newsImg"></img>
+            <img className='news-img' src={urlToImage} alt="newsImg"></img>
             <h3><a href={url}>{title}</a></h3>
             <p>{description}</p>
+            <p className='date'>{publishedAt.substring(0, publishedAt.length - 10)}</p>
         </div>
     )
 }
--- webapp/src/components/NewsList.js
+++ webapp/src/components/NewsList.js
@@ -13,16 +13,16 @@ const NewsList = () => {
             const res = await Axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=79e3de397dbb4204aec41d093348749c");
 
             setArticles(res.data.articles);
-            console.log(res)
+            
         }
 
         getArticles();
 
     }, []);
     return (
-        <div>
-            {articles.map(({ title, description, url, urlToImg }) => (
-                <NewsItem title={title} description={description} url={url} urlToImg={urlToImg} />
+        <div className='news-list'>
+            {articles.map(({ title, description, url, urlToImage, publishedAt }) => (
+                <NewsItem title={title} description={description} url={url} urlToImage={urlToImage} publishedAt= {publishedAt} />
             ))}
         </div>
     )
